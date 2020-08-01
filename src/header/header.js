7e2fa8b8ffd69a5a1127c62e6a67d9498d3d451b
Thai Nguyen <thai.nguyen@vn.woowahan.com>
2020-08-01 20:51:25 +0700

    init project

---
+++ src/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+import Menu from './menu';
+
+function Header(){
+    return(
+        <div>
+            
+            <Menu></Menu>
+        </div>
+    )
+}
+
+export default Header;
